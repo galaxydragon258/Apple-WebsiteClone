0a32e7252efa4de79b912d580dfbac45ead93d1b
YourCorrectUsername <galaxydragon259@gmail.com>
2026-04-23 14:33:50 +0800

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,40 @@
+import { navLists } from "../constant/index.js";
 
-import { navLists } from "../constant/index.js"
 const Navbar = () => {
     return (
-        <header className="w-full py-5 px-5 sm:px-10 flex justify-between items-center">
-            <nav className="flex w-full screen-max-width">
-                <img src="/assets/images/apple.svg" alt="Apple" width={14} height={18} />
-
-                <div className="flex flex-1 justify-center  max-sm:hidden">
+        <header className="w-full py-5 px-5 sm:px-10 flex justify-center">
+            <nav className="w-full max-w-7xl flex items-center justify-between">
+                <img
+                    src="/assets/images/apple.svg"
+                    alt="Apple"
+                    className="w-6 h-6 object-contain"
+                />
+                <div className="hidden sm:flex gap-8 text-sm text-gray-400">
                     {navLists.map((nav, i) => (
-                        <div key={i} className=" px-5 text-sm cursor-pointer text-gray-500 hover:text-white transition-colors duration-300">
+                        <div
+                            key={i}
+                            className="cursor-pointer hover:text-white transition"
+                        >
                             {nav}
                         </div>
                     ))}
                 </div>
-
-                <div className="flex items-baseline gap-7 max-sm:flex-1">
-                    <img src="/assets/images/search.svg" alt="Search" width={18} height={18} />
-                    <img src="/assets/images/bag.svg" alt="Bag" width={18} height={18} />
+                <div className="flex items-center gap-6">
+                    <img
+                        src="/assets/images/search.svg"
+                        alt="Search"
+                        className="w-4 h-4 cursor-pointer"
+                    />
+                    <img
+                        src="/assets/images/bag.svg"
+                        alt="Bag"
+                        className="w-4 h-4 cursor-pointer"
+                    />
                 </div>
+
             </nav>
         </header>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
